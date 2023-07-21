25a3d8620de6824a951e5e5b7f175cb42df82535
Giorgio Vilardo <giorgio@vilardo.eu>
2023-07-21 10:41:41 +0200

    add race conditio...huh sorry reset

--- main.go
+++ main.go
@@ -7,24 +7,40 @@ import (
 	"time"
 )
 
-func dbStatusCheck() func(w http.ResponseWriter, r *http.Request) {
-	var lastTime time.Time
+func dbStatusCheck(lastTime *time.Time) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
-			response := []byte(fmt.Sprintf("Last seen at %v", lastTime))
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			response := []byte(fmt.Sprintf("<h2>Last seen at %v</h2>", *lastTime))
 			w.Write(response)
 		case "POST":
-			lastTime = time.Now()
+			*lastTime = time.Now()
 			w.Write([]byte("Seen"))
 		default:
-			w.Write([]byte("Sorry, only GET and POST methods are supported."))
+			w.Write([]byte("Sorry"))
+		}
+	}
+}
+
+func resetStatusDb(lastTime *time.Time) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			*lastTime = time.Time{}
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			response := []byte("<h2>Reset</h2>")
+			w.Write(response)
+		default:
+			w.Write([]byte("Sorry"))
 		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/status", dbStatusCheck())
+	var lastTime time.Time
+	http.HandleFunc("/status", dbStatusCheck(&lastTime))
+	http.HandleFunc("/reset", resetStatusDb(&lastTime))
 
 	fmt.Printf("Starting server!")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
